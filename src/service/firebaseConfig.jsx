5eb8bb9026eddf2bd558221d063c7e8cc46c22e1
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-04-11 14:08:47 -0400

    Updated trip creation flow, firebase config, and environment variables

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,23 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {getFirestore} from "firebase/firestore"
+
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDw_h6J9oAg_2HdpsDpPtsKIn-v0qOV14M",
-  authDomain: "tubeguruji-startups.firebaseapp.com",
-  projectId: "tubeguruji-startups",
-  storageBucket: "tubeguruji-startups.appspot.com",
-  messagingSenderId: "706430327770",
-  appId: "1:706430327770:web:08482219ed803a0aad3ee3",
-  measurementId: "G-40ZKDR29LV"
+  apiKey: "AIzaSyBtj6omfpv7AEhDDexYEmSskMkuN5N69s8",
+  authDomain: "ai-trip-planner-2f557.firebaseapp.com",
+  projectId: "ai-trip-planner-2f557",
+  storageBucket: "ai-trip-planner-2f557.firebasestorage.app",
+  messagingSenderId: "110758122595",
+  appId: "1:110758122595:web:0eef38477a5ba516205456",
+  measurementId: "G-X8JTGXSPXP"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db=getFirestore(app);
+export const db=getFirestore()
 // const analytics = getAnalytics(app);
