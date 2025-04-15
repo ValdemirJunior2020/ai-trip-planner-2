5fea59a587aedfc2feb33bbaf036dd10bd256eb6
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-04-15 15:59:20 -0400

    text white for everything

--- src/view-trip/components/PlaceCardItem.jsx
+++ src/view-trip/components/PlaceCardItem.jsx
@@ -1,44 +1,46 @@
-import { Button } from '@/components/ui/button'
+
 import { GetPlaceDetails, PHOTO_REF_URL } from '@/service/GlobalApi';
-import React, { useEffect, useState } from 'react'
-import { FaMapLocationDot } from "react-icons/fa6";
+import React, { useEffect, useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
-function PlaceCardItem({place}) {
+function PlaceCardItem({ place }) {
+  const [photoUrl, setPhotoUrl] = useState();
 
-  const [photoUrl,setPhotoUrl]=useState();
-  useEffect(()=>{
-    place&&GetPlacePhoto();
-  },[place])
+  useEffect(() => {
+    place && GetPlacePhoto();
+  }, [place]);
 
-  const GetPlacePhoto=async()=>{
-    const data={
-      textQuery:place.placeName
-    }
-    const result=await GetPlaceDetails(data).then(resp=>{
-      const PhotoUrl=PHOTO_REF_URL.replace('{NAME}',resp.data.places[0].photos[3].name);
+  const GetPlacePhoto = async () => {
+    const data = {
+      textQuery: place.placeName,
+    };
+    const result = await GetPlaceDetails(data).then((resp) => {
+      const PhotoUrl = PHOTO_REF_URL.replace('{NAME}', resp.data.places[0].photos[3].name);
       setPhotoUrl(PhotoUrl);
-    })
-  }
+    });
+  };
 
   return (
-    <Link to={'https://www.google.com/maps/search/?api=1&query='+place.placeName} target='_blank'>
-        <div className='border rounded-xl p-3 mt-2 flex gap-5 
-        hover:scale-105 transition-all hover:shadow-md cursor-pointer'>
-            <img src={photoUrl?photoUrl:'/placeholder.jpg'}
-            className='w-[130px] h-[130px] rounded-xl object-cover'
-            />
-            <div>
-                <h2 className='font-bold text-lg'>{place.placeName}</h2>
-                <p className='text-sm text-white'>{place.placeDetails}</p>
+    <Link to={`https://www.google.com/maps/search/?api=1&query=${place.placeName}`} target="_blank">
+      <div className="border rounded-xl p-3 mt-2 flex gap-5 hover:scale-105 transition-all hover:shadow-md cursor-pointer">
+        <img
+          src={photoUrl ? photoUrl : '/placeholder.jpg'}
+          className="w-[130px] h-[130px] rounded-xl object-cover"
+          alt={place.placeName}
+        />
+        <div className="text-white">
+          <h2 className="font-bold text-lg">{place.placeName}</h2>
+          <p className="text-sm">{place.placeDetails}</p>
 
-                <h2 className='mt-2'>🕙 {place.timeToTravel}</h2>
-                <h2 className='mt-2'>🎟️ {place.ticketPricing}</h2>
-                {/* <Button size="sm"><FaMapLocationDot /></Button> */}
-            </div>
+          <h2 className="mt-2">🕙 {place.timeToTravel}</h2>
+          <h2 className="mt-2">🎟️ {place.ticketPricing}</h2>
+
+          {/* <Button size="sm"><FaMapLocationDot /></Button> */}
         </div>
+      </div>
     </Link>
-  )
+  );
 }
 
-export default PlaceCardItem
+export default PlaceCardItem;
