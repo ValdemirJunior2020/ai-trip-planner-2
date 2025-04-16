42552ae31dca3dc3f47dfcfd44a8eb1685f41946
ValdemirJunior2020 <your-infojr.83@gmail.com>
2025-04-16 09:59:27 -0400

    changed texts to white

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -107,8 +107,12 @@ function CreateTrip() {
 
   return (
     <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🏕️🌴</h2>
-      <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.</p>
+      <h2 className="text-3xl font-bold mb-6 text-white">Tell us your travel preferences 🏕️🌴</h2>
+
+      <p className='mt-3 text-white text-xl'>
+  Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences.
+</p>
+
 
       <div className='mt-20 flex flex-col gap-10'>
         <div>
